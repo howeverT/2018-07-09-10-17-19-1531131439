767cbb60d41f4f4703130cc285276b0aea279b10
HoweverT <1413537501@qq.com>
2020-07-22 20:21:31 +0800

    [oo-basic][david] feature: pass the practice06 test.

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,36 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private int klass;
+
+    public Teacher(String name, int age,int klass) {
+        super(name, age);
+        this.klass=klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        String result=super.introduce()+" I am a Teacher. ";
+        if (this.klass==0){
+            result+="I teach No Class.";
+        }
+        else {
+            result+="I teach Class "+this.klass+".";
+        }
+
+        return result;
+    }
+
 }
