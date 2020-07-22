10c31316094752b1ca41a7dc86f3c4e036b49475
HoweverT <1413537501@qq.com>
2020-07-22 22:33:18 +0800

    [oo-basic][david] feature: pass the practice09 test.

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,63 @@
 package practice09;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+
+    private int number;
+    private String displayName;
+    private Student leader;
+    private List<Student> studentList=new ArrayList<Student>();
+
+    public Klass(int number) {
+        this.number = number;
+        this.displayName="Class "+number;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void appendMember(Student student){
+        this.studentList.add(student);
+
+    }
+
+    public void assignLeader(Student student){
+        if (this.studentList.size()==0)
+            System.out.print("It is not one of us.\n");
+        else
+            this.setLeader(student);
+    }
+
 }
