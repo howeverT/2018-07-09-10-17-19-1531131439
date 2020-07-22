4d5b9c2a39a11bfbc9c9f3ff35b6b214ebcec42f
howevert <1413537501@qq.com>
2020-07-22 17:18:57 +0800

    [oo-basic][david] feature: pass the practice05 test.

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,14 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce(){
+        practice04.Person person=new practice04.Person(this.name,this.age);
+        return super.introduce()+" I am a Worker. I have a job.";
+    }
+
 }
