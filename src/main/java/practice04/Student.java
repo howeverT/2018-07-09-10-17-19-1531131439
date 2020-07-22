66474b2b2df200709f7661d78eb07f2fb7d78137
howevert <1413537501@qq.com>
2020-07-22 17:12:48 +0800

    [oo-basic][david] feature: pass the practice04 test.

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,23 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+
+    public Student(String name, int age,int klass) {
+        super(name, age);
+        this.klass=klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        Person person=new Person(this.name,this.age);
+        return person.basicIntroduce()+" I am a Student. I am at Class "+this.klass+".";
+    }
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,14 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce(){
+        Person person=new Person(this.name,this.age);
+        return person.basicIntroduce()+" I am a Worker. I have a job.";
+    }
+
 }
