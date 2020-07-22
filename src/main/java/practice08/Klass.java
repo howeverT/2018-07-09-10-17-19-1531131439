5d1fc208d988b8a4ded6f17b715bcc071a1b1416
HoweverT <1413537501@qq.com>
2020-07-22 21:39:12 +0800

    [oo-basic][david] feature: pass the practice08 test.

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,42 @@
 package practice08;
 
 public class Klass {
+
+    private int number;
+    private String displayName;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+        this.displayName="Class "+number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void assignLeader(Student student){
+        this.setLeader(student);
+    }
+
 }
