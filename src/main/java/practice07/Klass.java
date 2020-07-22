037969a0254b093e3b409fe4aaad7134a4434768
HoweverT <1413537501@qq.com>
2020-07-22 20:53:38 +0800

    [oo-basic][david] feature: pass the practice07 test.

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,27 @@
 package practice07;
 
 public class Klass {
+    private int number;
+    private String displayName;
+
+    public Klass(int number) {
+        this.number = number;
+        this.displayName="Class "+number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,53 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Klass klass;
+
+    public Teacher(String name, int age ,Klass klass) {
+        super(name, age);
+        this.klass=klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String headIntroduce(){
+        return super.introduce()+" I am a Teacher. ";
+    }
+
+    public String introduce(){
+        String result=this.headIntroduce();
+        if (this.klass==null){
+            result+="I teach No Class.";
+        }
+        else {
+            result+="I teach "+this.klass.getDisplayName()+".";
+        }
+
+        return result;
+    }
+
+    public String introduceWith(Student student){
+        int studentClass=student.getKlass().getNumber();
+        int teacherClass=this.klass.getNumber();
+        String result=this.headIntroduce();
+        if (studentClass==teacherClass){
+            result+="I teach "+student.getName()+".";
+        }
+        else {
+            result+="I don't teach "+student.getName()+".";
+        }
+        return result;
+    }
+
 }
